91e8c541e2b2dda87b9f159ee6896f5cba5f0b8a
zu1el <the.zufel@gmail.com>
2024-06-12 00:52:22 +0300

    login func

--- main.py
+++ main.py
@@ -1,68 +1,18 @@
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QPixmap
 import sys
 from localization import local
 
+current_lang = "en"
 ua = local("localization/ua.json")
 en = local("localization/en_login.json")
 
 App = QApplication(sys.argv)
-App.setStyleSheet("""
-    QWidget {
-        background-image: url("data/background.jpg");
-        background-repeat: no-repeat; 
-        background-position: center;
-        color: #FFFFFF;
-        border: 0px;
-        font-size: 35px;
-    }
-    QSplitter {
-        opacity: 0;
-    }
-    
-    QPushButton {
-    color: #FFFFFF;
-    border-radius: 10px;
-    border-style: outset;
-    background: #222222;
-    width: 340;
-    height: 45;
-    font-size: 25;
-    padding: 5px;
-    }
-
-    QPushButton:hover {
-        background: #191919
-    }
-    
-    QPushButton:pressed {
-        border-style: inset;
-        background: #151515
-    }
-    
-    QLineEdit {
-    color: #FFFFFF;
-    border-radius: 10px;
-    border-style: outset;
-    background: #222222;
-    width: 340;
-    height: 45;
-    font-size: 25px;
-    padding: 10px;
-    }
-""")
-
-spliter = QSplitter()
-
-opacity_effect = QGraphicsOpacityEffect()
-opacity_effect.setOpacity(0)
-spliter.setGraphicsEffect(opacity_effect)
 
 login = QWidget()
-login.resize(480, 720)
+login.setStyleSheet("""background-image: url("data/background.jpg"); background-repeat: no-repeat; background-position: center; color: #FFFFFF; border: 0px; font-size: 35px;""")
+login.setFixedSize(440, 700)
 
-login_photo_line = QHBoxLayout()
-login_v_line = QVBoxLayout()
+loginMainLine = QVBoxLayout()
 label_line = QHBoxLayout()
 login_line = QHBoxLayout()
 password_line = QHBoxLayout()
@@ -73,24 +23,22 @@ chek_line = QHBoxLayout()
 login_label = QLabel(en.get("login"))
 login_label.setStyleSheet("""font-size: 45px; background:rgba(0, 0, 0, 0);""")
 
-login_photo_widget = QLabel()
 login_input = QLineEdit()
 password_input = QLineEdit()
+for widget in [login_input, password_input]: widget.setStyleSheet("""color: #FFFFFF; border-radius: 10px; border-style: outset; background: #222222; width: 340; height: 45; font-size: 25px; padding: 10px;""")
 login_input.setPlaceholderText(en.get("ent-login"))
 password_input.setPlaceholderText(en.get("ent-password"))
 sing_in = QPushButton(en.get("sing-in"))
 sing_up = QPushButton(en.get("sing-up"))
-dark_mode = QCheckBox(en.get("dark-mode"))
+for widget in [sing_in, sing_up]: widget.setStyleSheet("""QPushButton {color: #FFFFFF; border-radius: 10px; border-style: outset; background: #222222; width: 340; height: 45; font-size: 25; padding: 5px;} QPushButton:hover {background: #191919} QPushButton:pressed {border-style: inset; background: #151515}""")
 ua_localization = QCheckBox(en.get("ua-localization"))
+ua_localization.setStyleSheet("""font-size: 16px; background:rgba(0, 0, 0, 0);""")
 
-pixmap = QPixmap("data/background.jpg")
-login_photo_widget.setPixmap(pixmap)
-
-login_photo_line.addWidget(login_photo_widget)
+spliter = QSplitter()
+spliter.setStyleSheet("""background:rgba(0, 0, 0, 0);""")
 
 chek_line.addWidget(spliter)
 chek_line.addWidget(ua_localization)
-chek_line.addWidget(dark_mode)
 chek_line.addWidget(spliter)
 
 label_line.addWidget(spliter)
@@ -113,23 +61,58 @@ sing_up_line.addWidget(spliter)
 sing_up_line.addWidget(sing_up)
 sing_up_line.addWidget(spliter)
 
-login_v_line.addWidget(spliter)
-login_v_line.addWidget(spliter)
-login_v_line.addWidget(spliter)
-login_v_line.addLayout(label_line)
-login_v_line.addWidget(spliter)
-login_v_line.addWidget(spliter)
-login_v_line.addWidget(spliter)
-login_v_line.addWidget(spliter)
-login_v_line.addWidget(spliter)
-login_v_line.addLayout(login_line)
-login_v_line.addLayout(password_line)
-login_v_line.addLayout(sing_in_line)
-login_v_line.addLayout(sing_up_line)
-login_v_line.addWidget(spliter)
-login_v_line.addLayout(chek_line)
+loginMainLine.addLayout(chek_line)
+loginMainLine.addWidget(spliter)
+loginMainLine.addWidget(spliter)
+loginMainLine.addWidget(spliter)
+loginMainLine.addLayout(label_line)
+loginMainLine.addWidget(spliter)
+loginMainLine.addWidget(spliter)
+loginMainLine.addWidget(spliter)
+loginMainLine.addWidget(spliter)
+loginMainLine.addWidget(spliter)
+loginMainLine.addLayout(login_line)
+loginMainLine.addLayout(password_line)
+loginMainLine.addLayout(sing_in_line)
+loginMainLine.addLayout(sing_up_line)
+loginMainLine.addWidget(spliter)
 
-login.setLayout(login_v_line)
+login.setLayout(loginMainLine)
+
+
+#------------------------------------
+
+
+game = QWidget()
+game.setFixedSize(440, 700)
+
+
+def localization():
+    global current_lang
+    if current_lang == "en":
+        current_lang = "ua"
+        login_label.setText(ua.get("login"))
+        login_input.setPlaceholderText(ua.get("ent-login"))
+        password_input.setPlaceholderText(ua.get("ent-password"))
+        sing_in.setText(ua.get("sing-in"))
+        sing_up.setText(ua.get("sing-up"))
+        ua_localization.setText(ua.get("ua-localization"))
+    else:
+        current_lang = "en"
+        login_label.setText(en.get("login"))
+        login_input.setPlaceholderText(en.get("ent-login"))
+        password_input.setPlaceholderText(en.get("ent-password"))
+        sing_in.setText(en.get("sing-in"))
+        sing_up.setText(en.get("sing-up"))
+        ua_localization.setText(en.get("ua-localization"))
+
+def logining(login_, password):
+    login.close()
+    game.show()
+
+ua_localization.clicked.connect(localization)
+
+sing_in.clicked.connect(lambda: logining("zufel", "password"))
 
 login.show()
 App.exec()
